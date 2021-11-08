6f6aa2fec70f3ad0a59fcb79235341b445329aa6
Polina <polinageiko10@gmail.com>
2021-11-08 12:16:39 -0400

    feat: implement code for 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,30 +1,41 @@
 
 const readline = require('readline');
-const rl = readline.createInterface({ input, output });
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const { open, close, appendFile } = require('fs');
 
-
 function closeFd(fd) {
-  close(fd, (err) => {
-    if (err) throw err;
-  });
+    close(fd, (err) => {
+        if (err) throw err;
+    });
+}     
+
+function fileRecord(rl, fd) {
+    rl.question('New line: ', (answer) => {
+
+                if (answer == 'exit') {
+                    console.log("The file is successfully closed. Thank you!")
+                    closeFd(fd);
+                    rl.close();
+                }
+                
+                else {
+                    appendFile(fd, `\n ${answer}`, 'utf8', (err) => {
+                        if (err) throw err;
+                    });
+                    fileRecord(rl, fd);
+                }
+            });
 }
 
-open('message.txt', 'a', (err, fd) => {
-  if (err) throw err;
 
-  try {
-    rl.question('Start your first line:', (answer) => {
-        console.log(`Thank you for your line: ${answer}`);
-        
-        appendFile(fd, answer, 'utf8', (err) => {
+console.log("Starting your new txt file!\n(To stop recording write 'exit' or press Ctrl+C)\n")
+    open('message.txt', 'a', (err, fd) => {
+        if (err) throw err;
+        fileRecord(rl, fd);
+
+        rl.on('SIGINT', function() {
+            console.log("\nThe file is successfully closed. Thank you!")
             closeFd(fd);
-            if (err) throw err;
-          });
-      });
-    
-  } catch (err) {
-    closeFd(fd);
-    throw err;
-  }
-});
+            rl.close();
+        });
+    });
