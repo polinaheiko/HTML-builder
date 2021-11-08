889e55e4dc974608fa3c793d2fd4fca492a7578e
Polina <polinageiko10@gmail.com>
2021-11-08 16:26:41 -0400

    feat: implement code for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const { RSA_NO_PADDING } = require('constants');
+const fs = require('fs');
+const { copyFile, constants } = require('fs');
 
+
+fs.mkdir('./04-copy-directory/files-copy', { recursive: true }, (err) => {
+    if (err) throw err;
+});
+
+
+fs.readdir('./04-copy-directory/files', 
+            { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    else {
+        files.forEach(file => {
+            copyFile(`./04-copy-directory/files/${file.name}`, `./04-copy-directory/files-copy/${file.name}`, (err) => {
+                if (err) throw err});
+        });
+    }
+});
